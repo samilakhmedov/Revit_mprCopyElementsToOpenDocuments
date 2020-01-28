71f610bc8f224d9a1e05d83f873c2d039268a5a3
Samil Akhmedov <akhmedov.s.a@yandex.ru>
2020-01-29 01:58:35 +0300

    [inProcess] elements copying

---
+++ mprCopyElementsToOpenDocuments/Helpers/CustomCopyHandlerAbort.cs
@@ -0,0 +1,16 @@
+﻿namespace mprCopyElementsToOpenDocuments.Helpers
+{
+    using Autodesk.Revit.DB;
+
+    /// <summary>
+    /// Обработчик копирования элементов
+    /// </summary>
+    public class CustomCopyHandlerAbort : IDuplicateTypeNamesHandler
+    {
+        /// <inheritdoc/>
+        public DuplicateTypeAction OnDuplicateTypeNamesFound(DuplicateTypeNamesHandlerArgs args)
+        {
+            return DuplicateTypeAction.Abort;
+        }
+    }
+}
--- mprCopyElementsToOpenDocuments/Helpers/RevitOperationService.cs
+++ mprCopyElementsToOpenDocuments/Helpers/RevitOperationService.cs
@@ -1,4 +1,6 @@
-﻿namespace mprCopyElementsToOpenDocuments.Helpers
+﻿using mprCopyElementsToOpenDocuments.Models.Interfaces;
+
+namespace mprCopyElementsToOpenDocuments.Helpers
 {
     using System;
     using System.Collections.Generic;
@@ -12,10 +14,15 @@
     /// </summary>
     public class RevitOperationService
     {
-        private const string ParamError = "Ошибка при получении элементов категории {0}";
-        private const string ParamStart = "{0} - Начало операции сбора элементов модели";
-        private const string ParamFinish = "{0} - Завершение операции сбора элементов модели";
+        private const string ParamError = "Ошибка при получении элементов категории {0}.";
+        private const string ParamStart = "{0} - Начало операции сбора элементов модели.";
+        private const string ParamFinish = "{0} - Завершение операции сбора элементов модели.";
+        private const string CopyStart = "{0} - Начало копирования элементов из документа \"{1}\" в документы \"{2}\".";
+        private const string CopyFinish = "{0} - Завершение копирования элементов из документа \"{1}\" в документы \"{2}\".";
+        private const string CopyElementError = "{0} - В процессе копирования элемента \"{1}\" категории \"{2}\" произошла ошибка: \"{3}\".";
+        private const string CopyingOptions = "Настройки копирования элементов: \"{0}\".";
         private readonly UIApplication _uiApplication;
+        private CopyPasteOptions _copyPasteOption = new CopyPasteOptions();
 
         /// <summary>
         /// Создает экземпляр класса <see cref="UIApplication"/>
@@ -59,7 +66,8 @@
             {
                 try
                 {
-                    var elements = group.Select(element => new BrowserItem(element.Name, element.Id.IntegerValue))
+                    var elements = group
+                        .Select(element => new BrowserItem(element.Name, element.Id.IntegerValue))
                         .ToList();
                     groups.Add(new BrowserItemGroup(group.Key, elements));
                 }
@@ -73,6 +81,83 @@
             Logger.Instance.Add("---------");
 
             return new BrowserGeneralGroup("Все группы", groups);
+        }
+
+        /// <summary>
+        /// Копирует все выбранные элементы в выбранные документы
+        /// </summary>
+        /// <param name="documentFrom">Документ из которого производится копирование</param>
+        /// <param name="documentsTo">Список документов в которые осуществляется копирование</param>
+        /// <param name="elements">Список элементов Revit</param>
+        /// <param name="copyingOptions">Настройки копирования элементов</param>
+        public void CopyAllElements(
+            RevitDocument documentFrom,
+            List<RevitDocument> documentsTo,
+            List<IRevitEntity> elements,
+            CopyingOptions copyingOptions)
+        {
+            Logger.Instance.Add(string.Format(CopyStart, DateTime.Now.ToLocalTime(), documentFrom.Title, string.Join(", ", documentsTo)));
+            Logger.Instance.Add(string.Format(CopyingOptions, GetCopyingOptionsName(copyingOptions)));
+
+            try
+            {
+                switch (copyingOptions)
+                {
+                    case Helpers.CopyingOptions.AllowDuplicates:
+                        _copyPasteOption.SetDuplicateTypeNamesHandler(new CustomCopyHandlerAllow());
+                        break;
+                    case Helpers.CopyingOptions.DisallowDuplicates:
+                        _copyPasteOption.SetDuplicateTypeNamesHandler(new CustomCopyHandlerAbort());
+                        break;
+                }
+
+                foreach (var element in elements)
+                {
+                    var elementId = new ElementId(element.Id);
+                    var revitElement = documentFrom.Document.GetElement(elementId);
+
+                    foreach (var documentTo in documentsTo)
+                    {
+                        if (revitElement.GetType() == typeof(Workset) && documentTo.Document.IsWorkshared)
+                        {
+                            Workset.Create(documentTo.Document, revitElement.Name);
+                        }
+                        else
+                        {
+                            // TODO: Logger event
+                        }
+                    }
+
+                    // TODO: General copying case
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Instance.Add(string.Format(CopyElementError, DateTime.Now.ToLocalTime(), "", "", e.Message));
+            }
+
+            Logger.Instance.Add(string.Format(CopyFinish, DateTime.Now.ToLocalTime(), documentFrom.Title, string.Join(", ", documentsTo)));
+            Logger.Instance.Add("---------");
+        }
+
+        /// <summary>
+        /// Возвращает текстовое представление настроек копирования элементов
+        /// </summary>
+        /// <param name="copyingOptions">Настройки копирования элементов</param>
+        /// <returns>Текстовое представление настроек копирования элементов</returns>
+        private string GetCopyingOptionsName(CopyingOptions copyingOptions)
+        {
+            switch (copyingOptions)
+            {
+                case Helpers.CopyingOptions.AllowDuplicates:
+                    return "Разрешить дублирование";
+                case Helpers.CopyingOptions.DisallowDuplicates:
+                    return "Запретить дублирование";
+                case Helpers.CopyingOptions.AskUser:
+                    return "Запросить разрешение";
+                default:
+                    return string.Empty;
+            }
         }
     }
 }
